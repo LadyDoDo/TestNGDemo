6e9f25fd8712bc984b215c9492bc02b0b4f592dd
unknown <fokui@wacai.com>
2019-06-10 19:32:38 +0800

    first commit.

---
+++ src/main/java/test/testng/demo/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package test.testng.demo;
+
+
+/**
+ * @author fokui
+ * @date 2019/5/28 19:36
+ */
+public class TestHelloWorld {
+    public String sayHi(){
+        return "Hello World.";
+    }
+}
---
+++ src/test/java/test/testng/demo/script/TestHelloWorldTest.java
@@ -0,0 +1,19 @@
+package test.testng.demo.script;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import test.testng.demo.TestHelloWorld;
+
+/**
+ * @author fokui
+ * @date 2019/5/29 11:16
+ */
+public class TestHelloWorldTest {
+    @Test
+    public void testcase(){
+        TestHelloWorld helloWorld = new TestHelloWorld();
+        String str = helloWorld.sayHi();
+        Assert.assertNotNull(str);
+        Assert.assertEquals(str, "Hello World.");
+    }
+}
